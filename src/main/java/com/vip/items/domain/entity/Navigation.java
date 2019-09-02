4f1cb19f60792edfdcecddec2aee1584ff87bbf1
e <13450107827@163.com>
2019-09-02 20:06:34 +0800

    轮播图接口

---
+++ src/main/java/com/vip/items/domain/entity/Navigation.java
@@ -0,0 +1,21 @@
+package com.vip.items.domain.entity;
+
+import lombok.Data;
+
+@Data
+public class Navigation {
+    /**
+    * 导航id
+    */
+    private Integer navId;
+
+    /**
+    * 导航名
+    */
+    private String navName;
+
+    /**
+    * 状态,0 存在,1删除
+    */
+    private Integer isDelete;
+}
---
+++ src/main/java/com/vip/items/mapper/NavigationMapper.java
@@ -0,0 +1,7 @@
+package com.vip.items.mapper;
+
+import com.vip.items.domain.entity.Navigation;
+
+public interface NavigationMapper {
+    Navigation findAll();
+}
